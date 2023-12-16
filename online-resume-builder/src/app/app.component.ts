eb1c4616b622460ec87b3d87d30ffce6cea337ce
Oneeba Ahsan <oneebaahsan8@gmail.com>
2023-12-16 19:21:48 +0500

    Installing initial libraries and initial FE page

--- online-resume-builder/src/app/app.component.ts
+++ online-resume-builder/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'online-resume-builder';
+  title: string = 'resume-builder';
 }
--- online-resume-builder/src/app/app.module.ts
+++ online-resume-builder/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ResumeBuilderComponent } from './resume-builder/resume-builder.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select'
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
+declarations: [AppComponent, ResumeBuilderComponent],
+imports: [BrowserModule, ReactiveFormsModule, BrowserAnimationsModule, FormsModule, HttpClientModule, NzFormModule, NzSelectModule, NzInputModule, NzSpaceModule],
+providers: [
+    { provide: NZ_I18N, useValue: en_US }
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
