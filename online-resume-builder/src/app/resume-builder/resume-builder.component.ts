251469098698c888b5dc8b920a7d2d3bab4de727
Oneeba Ahsan <oneebaahsan8@gmail.com>
2023-12-16 20:10:26 +0500

    adding pdf generation function and its libraries

--- online-resume-builder/src/app/resume-builder/resume-builder.component.ts
+++ online-resume-builder/src/app/resume-builder/resume-builder.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray, ReactiveFormsModule, ValidationErrors } from '@angular/forms';
+import { AbstractControl,FormControl,NonNullableFormBuilder,ValidatorFn} from '@angular/forms';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-resume-builder',
@@ -153,6 +157,190 @@ export class ResumeBuilderComponent implements OnInit {
   //remove skills block
   removeSkills(index: number){
     (this.resumeFormBuilder.get('skills') as FormArray).removeAt(index);
+  }
+
+  // to convert the FormBuilder object into JSON format, for readability
+  saveResume(): void {
+    console.log(JSON.stringify(this.resumeFormBuilder.value, null, 2));
+
+  }
+
+  // to generate the PDF from the form group
+  onGeneratePdfClick(): void {
+    this.generatePdfFromFormGroup(this.resumeFormBuilder);
+  }
+
+  // binded in HTML, to generate the PDF of the form Group
+  onFormSubmit(): void {
+    if (this.resumeFormBuilder.valid) {
+      this.generatePdfFromFormGroup(this.resumeFormBuilder);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  // generate the PDF from the form group and style the content inside it
+  generatePdfFromFormGroup(formGroup: FormGroup): void {
+    const formData = formGroup.value;
+
+    const content = [];
+
+    const linksSection: { width: string; alignment: string; fontSize: number; stack: any[] } = {
+      width: 'auto',
+      alignment: 'right',
+      fontSize: 12,
+      stack: []
+    };
+
+    // LinkedIn link
+    linksSection.stack.push({
+      stack: [
+        {
+          text: 'LinkedIn',
+          link: formData.linkedInLink,
+          color: 'blue',
+          fontSize: 12,
+          margin: [0, 0, 0, 10]
+        }
+      ]
+    });
+
+  // GitHub link
+    linksSection.stack.push({
+      stack: [
+        {
+          text: 'GitHub',
+          link: formData.githubLink,
+          color: 'blue',
+          fontSize: 12,
+          margin: [0, 0, 0, 10]
+        }
+      ]
+    });
+
+  // Optional link, check is optional link is provided
+  if (formData.optionalLink != null && formData.optionalLink != '') {
+    linksSection.stack.push({
+      stack: [
+        {
+          text: 'Others',
+          link: formData.optionalLink,
+          color: 'blue',
+          fontSize: 12,
+          margin: [0, 0, 0, 10]
+        }
+      ]
+    });
+  }
+
+  // Add links section to content
+    content.push({
+      columns: [
+        {
+          width: '*',
+          text: `${formData.firstName} ${formData.lastName}`,
+          fontSize: 20,
+          bold: true
+        },
+        {
+          width: 'auto',
+          alignment: 'right', // Aligns content to the right
+          stack: [linksSection], // Place linksSection inside another stack for alignment
+          margin: [0, 20, 0, 0] // Adjust margin as needed
+        }
+      ],
+      margin: [0, 0, 0, 15]
+    });
+
+
+    // Add remaining form Groups to content along with their styling
+    content.push(
+      {
+        text: 'Contact Information',
+        style: 'sectionHeader',
+        fontSize: 12,
+        fontStyle: 'italic'
+      },
+      {
+        columns: [
+          { width: 'auto', text: 'Email:', bold: true },
+          { width: '*', text: formData.email },
+          { width: 'auto', text: 'Contact:', bold: true },
+          { width: '*', text: formData.phone },
+          { width: 'auto', text: 'Country:', bold: true },
+          { width: '*', text: formData.country }
+        ],
+        columnGap: 6,
+        margin: [0, 0, 0, 15],
+        fontSize: 10
+      },
+      {
+        text: 'Education',
+        style: 'sectionHeader',
+        fontSize: 12,
+        fontStyle: 'italic'
+      },
+      {
+        ul: formData.education.map((education: any) => ({
+          text: `${education.degree} in ${education.fieldOfStudy} at ${education.school} (${education.startYear} to ${education.endYear})`,
+          fontSize: 10
+        })),
+        margin: [0, 0, 0, 15],
+      },
+      {
+        text: 'Experience',
+        style: 'sectionHeader',
+        fontSize: 12,
+        fontStyle: 'italic'
+      },
+      {
+        ul: formData.experience.map((experience: any) => ({
+          text: `${experience.position} at ${experience.company} (${experience.startYear} - ${experience.endYear}) - ${experience.description}`,
+          fontSize: 10
+        })),
+        margin: [0, 0, 0, 15],
+      },
+      {
+        text: 'Projects',
+        style: 'sectionHeader',
+        fontSize: 12,
+        fontStyle: 'italic'
+      },
+      {
+        ul: formData.projects.map((projects: any) => ({
+          text: `${projects.projectName} - ${projects.projectDescription}`,
+          fontSize: 10
+        })),
+        margin: [0, 0, 0, 15],
+      },
+      {
+        text: 'Skills',
+        style: 'sectionHeader',
+        fontSize: 12,
+        fontStyle: 'italic'
+      },
+      {
+        ul: formData.skills.map((skills: any) => ({
+          text: `${skills.name} - ${skills.level}`,
+          fontSize: 10
+        })),
+        margin: [0, 0, 0, 15],
+      }
+    );
+
+    const docDefinition: any = {
+      content:
+      content,
+      styles: {
+        sectionHeader: {
+          bold: true,
+          margin: [0, 15, 0, 10]
+        }
+      }
+    };
+
+    // download the PDF
+    pdfMake.createPdf(docDefinition).download(formData.firstName + '_' + formData.lastName + '_resume.pdf');
   }
 
 
